0157c598e7bc08fd7f5cb559adacbfd857699623
kplaystart <161541943+kplaystart@users.noreply.github.com>
2024-03-29 18:02:38 -0600

    Added jquery to the createGrid function in order to determine heiht and width of grid squares through  a formula (diving the board size and the user input number)

--- myScript.js
+++ myScript.js
@@ -1,14 +1,22 @@
 //const board = document.getElementById('#board')
 document.getElementById("btn").addEventListener("click", defaultGrid);
 
+document.getElementById("btn").addEventListener("click", clearGrid);
+
+
 function defaultGrid(){
 
+let squares = document.getElementById("input").value
 let userInput = document.getElementById("input").value **2;
 
+
 for (i=0; i< userInput; i++){
-  
+
   let myDiv = document.querySelector("#board");
 let littleDivs = document.createElement("div");
+$(littleDivs).width(400/squares);
+$(littleDivs).height(400/squares);
+littleDivs.style.aspectRatio = '1/1'
 littleDivs.classList.add("mystyle");
 myDiv.appendChild(littleDivs);
  
@@ -16,6 +24,12 @@ myDiv.appendChild(littleDivs);
    
 }
 defaultGrid();
+
+
+
+function clearGrid(){
+  $("littleDivs").remove();
+};  
 
 
 
